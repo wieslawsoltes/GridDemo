4b939f6fbdecc335c892234bdef7ec988708f66d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2021-05-18 15:59:27 +0200

    Update MainWindowViewModel.cs

--- GridDemo/ViewModels/MainWindowViewModel.cs
+++ GridDemo/ViewModels/MainWindowViewModel.cs
@@ -37,9 +37,9 @@ namespace GridDemo.ViewModels
 		{
 			Layouts = new ObservableCollection<TileLayoutViewModel>()
 			{
-				new TileLayoutViewModel("Small", "228,228,228,228,228", "126"),
-				new TileLayoutViewModel("Normal", "228,228,228", "126,252"),
-				new TileLayoutViewModel("Wide", "228,228", "126,252,252"),
+				new("Small", "228,228,228,228,228", "126"),
+				new("Normal", "228,228,228", "126,252"),
+				new("Wide", "228,228", "126,252,252")
 			};
 
 			LayoutIndex = 1;
@@ -47,62 +47,62 @@ namespace GridDemo.ViewModels
 			Tiles = new ObservableCollection<TileViewModel>()
 			{
 				// 0
-				new TileViewModel()
+				new()
 				{
 					Background = Brushes.Red,
 					TilePresets = new ObservableCollection<TilePresetViewModel>()
 					{
-						new TilePresetViewModel(0, 0, 1, 1),
-						new TilePresetViewModel(0, 0, 1, 1),
-						new TilePresetViewModel(0, 0, 1, 1),
+						new(0, 0, 1, 1),
+						new(0, 0, 1, 1),
+						new(0, 0, 1, 1)
 					},
 					TilePresetIndex = LayoutIndex
 				},
 				// 1
-				new TileViewModel()
+				new()
 				{
 					Background = Brushes.Green,
 					TilePresets = new ObservableCollection<TilePresetViewModel>()
 					{
-						new TilePresetViewModel(1, 0, 1, 1),
-						new TilePresetViewModel(1, 0, 1, 1),
-						new TilePresetViewModel(1, 0, 1, 1),
+						new(1, 0, 1, 1),
+						new(1, 0, 1, 1),
+						new(1, 0, 1, 1)
 					},
 					TilePresetIndex = LayoutIndex
 				},
 				// 2
-				new TileViewModel()
+				new()
 				{
 					Background = Brushes.Blue,
 					TilePresets = new ObservableCollection<TilePresetViewModel>()
 					{
-						new TilePresetViewModel(2, 0, 1, 1),
-						new TilePresetViewModel(2, 0, 1, 1),
-						new TilePresetViewModel(0, 1, 1, 1),
+						new(2, 0, 1, 1),
+						new(2, 0, 1, 1),
+						new(0, 1, 1, 1)
 					},
 					TilePresetIndex = LayoutIndex
 				},
 				// 3
-				new TileViewModel()
+				new()
 				{
 					Background = Brushes.Yellow,
 					TilePresets = new ObservableCollection<TilePresetViewModel>()
 					{
-						new TilePresetViewModel(3, 0, 1, 1),
-						new TilePresetViewModel(0, 1, 1, 1),
-						new TilePresetViewModel(1, 1, 1, 1),
+						new(3, 0, 1, 1),
+						new(0, 1, 1, 1),
+						new(1, 1, 1, 1)
 					},
 					TilePresetIndex = LayoutIndex
 				},
 				// 4
-				new TileViewModel()
+				new()
 				{
 					Background = Brushes.Black,
 					TilePresets = new ObservableCollection<TilePresetViewModel>()
 					{
-						new TilePresetViewModel(4, 0, 1, 1),
-						new TilePresetViewModel(1, 1, 2, 1),
-						new TilePresetViewModel(0, 2, 2, 1),
+						new(4, 0, 1, 1),
+						new(1, 1, 2, 1),
+						new(0, 2, 2, 1)
 					},
 					TilePresetIndex = LayoutIndex
 				},
